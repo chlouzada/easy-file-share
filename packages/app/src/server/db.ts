b61f0d8f998ffa3b1fa4adc9216ae9f249ead1d6
chlouzada <chlouzada@gmail.com>
2023-05-23 18:43:33 -0300

    remove old file api

--- packages/app/src/server/db.ts
+++ packages/app/src/server/db.ts
@@ -2,5 +2,4 @@ import { MongoClient } from 'mongodb';
  
 const client = new MongoClient(process.env.MONGO_URL!)
 
-export const FileCollection = client.db('easy-file-share').collection('files');
 export const TunnelCollection = client.db('easy-file-share').collection('tunnels');
