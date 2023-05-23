dadd75e2329fd82335021423363e196dd196a7f7
chlouzada <chlouzada@gmail.com>
2023-05-23 20:25:26 -0300

    create options on create tunnel fn

--- packages/app/src/app/api/tunnels/route.ts
+++ packages/app/src/app/api/tunnels/route.ts
@@ -3,22 +3,27 @@ import { z } from 'zod';
 import { nanoid } from 'nanoid';
 import { TunnelCollection } from '@/server/db';
 
+const DEFAULT_LENGTH = 5;
+
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const validated = z
       .object({
         url: z.string(),
+        options: z
+          .object({
+            keyLenght: z.number().optional(),
+          })
+          .optional(),
       })
       .parse(body);
-
     const getKey = (): string => {
-      const key = nanoid(5);
+      const key = nanoid(validated.options?.keyLenght ?? DEFAULT_LENGTH);
       if (key.startsWith('-')) return getKey();
       return key;
     };
     const key = getKey();
-
     const { insertedId } = await TunnelCollection.insertOne({
       key,
       ...validated,
