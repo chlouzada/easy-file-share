c6785610304dba6f389e6f8c7d1035b4e8925df3
chlouzada <chlouzada@gmail.com>
2023-05-23 23:09:00 -0300

    organize fn on cli

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -2,81 +2,16 @@
 // @ts-check
 
 import { program } from 'commander';
-import { createReadStream, readdirSync, createWriteStream } from 'fs';
-import { createServer } from 'http';
-import { join } from 'path';
-import { execa } from 'execa';
+import { createWriteStream } from 'fs';
 import axios from 'axios';
 import inquirer from 'inquirer';
 
 import { tunnel } from './helpers/tunnel.js';
+import { server } from './helpers/server.js';
 
 const PORT = 8080;
 
-program
-  .command('serve')
-  .description('Serve current directory')
-  .action(async () => {
-    try {
-      const server = createServer((req, res) => {
-        /** @type {any} */
-        const url = req.url;
-        if (url === '/') {
-          const filenames = readdirSync(process.cwd());
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(filenames));
-          return;
-        }
-        const filePath = join(process.cwd(), url);
-        const fileStream = createReadStream(filePath);
-        fileStream.on('error', (error) => {
-          res.writeHead(404, { 'Content-Type': 'text/plain' });
-          res.end('File not found');
-        });
-        res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
-        fileStream.pipe(res);
-      });
-      server.listen(PORT, async () => {
-        const stream = execa('ssh', [
-          '-R',
-          `80:localhost:${PORT}`,
-          'localhost.run',
-        ]).stdout;
-        if (!stream) {
-          console.error('Error creating tunnel');
-          process.exit(1);
-        }
-        /** @type {string | null} */
-        let id = null;
-        stream.on('data', async (chunk) => {
-          /** @type {string[] | null} */
-          const match = chunk
-            .toString()
-            .match(
-              /\bhttps?:\/\/(?:[\w-]+\.)*[\w-]+(?:\.[a-zA-Z]{2,})+(?:\/(?:[^\s/$.?#]+\/?)*(?:\?[^\s/?#]+)?(?:#[^\s#]*)?)?/
-            );
-          if (!match || !match.length) {
-            console.log(chunk.toString());
-            console.error("Couldn't parse tunnel url");
-            process.exit(1);
-          }
-          /** @type {string} */
-          const url = match[0];
-          if (!id) {
-            console.log('Tunnel created successfully');
-            const created = await tunnel.create(url);
-            id = created.id;
-            console.log(`\nKey: ${created.key}`);
-          } else {
-            tunnel.update(id, url);
-          }
-          console.log(`URL: ${url}`);
-        });
-      });
-    } catch (error) {
-      console.error(`Error uploading file: ${error}`);
-    }
-  });
+program.command('serve').description('Serve current directory').action(server);
 
 program
   .command('pull <key/url>')
