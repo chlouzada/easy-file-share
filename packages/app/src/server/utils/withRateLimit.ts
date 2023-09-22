caf36c5bad829514e99b074bff3375bc6e57b714
chlouzada <chlouzada@gmail.com>
2023-09-22 16:15:04 -0300

    upstash redis client

--- packages/app/src/server/utils/withRateLimit.ts
+++ packages/app/src/server/utils/withRateLimit.ts
@@ -1,17 +1,27 @@
 import { NextResponse, NextRequest } from 'next/server';
-import { kv } from '@vercel/kv';
 import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
+              
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_URL!,
+  token: process.env.UPSTASH_REDIS_TOKEN!,
+})
 
 const rl = new Ratelimit({
-  redis: kv,
+  redis,
   limiter: Ratelimit.slidingWindow(3, '1 s'),
 });
 
 export const withRateLimit =
   (cb: (req: NextRequest) => Promise<NextResponse>) =>
   async (req: NextRequest) => {
+    console.time('1');
     const ip = req.headers.get('x-forwarded-for') ?? 'DEFAULT';
-    const { success, reset } = await rl.limit(ip);
+    const data = await rl.limit(ip);
+    console.log(data);
+    const { success, reset } = data;
+    console.timeEnd('1');
+
     if (success) {
       return cb(req);
     }
